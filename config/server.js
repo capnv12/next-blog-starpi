5c6dfb0097227dafb11727b0c95caa01981649ed
capn12 <capn12@gmail.com>
2021-01-11 16:02:40 +0200

    correct server config

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "206.189.48.12"),
+  host: env("HOST", "159.89.97.246"),
   port: env.int("PORT", 1337),
   admin: {
-    url: "/admin",
+    url: "/dashboard",
     auth: {
       secret: env("ADMIN_JWT_SECRET", "945fcb0ac64320ce536e5506efd7b10a"),
     },
