ef1fb5dc51ed4b0e98bdad56e602c8dcada0f9c1
capn12 <capn12@gmail.com>
2021-01-16 17:02:16 +0200

    aaa

--- config/database.js
+++ config/database.js
@@ -1,40 +1,19 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connections: {
-//     default: {
-//       connector: "mongoose",
-//       settings: {
-//         host: env("DATABASE_HOST", "localhost"),
-//         srv: env.bool("DATABASE_SRV", false),
-//         port: env.int("DATABASE_PORT", 27017),
-//         database: env("DATABASE_NAME", "next-blog"),
-//         username: env("DATABASE_USERNAME", null),
-//         password: env("DATABASE_PASSWORD", null),
-//       },
-//       options: {
-//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-//         ssl: env.bool("DATABASE_SSL", false),
-//       },
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST"),
-        srv: env.bool("DATABASE_SRV", true),
+        host: env("DATABASE_HOST", "localhost"),
+        srv: env.bool("DATABASE_SRV", false),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        database: env("DATABASE_NAME", "next-blog"),
+        username: env("DATABASE_USERNAME", null),
+        password: env("DATABASE_PASSWORD", null),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        ssl: env.bool("DATABASE_SSL", true),
+        ssl: env.bool("DATABASE_SSL", false),
       },
     },
   },
