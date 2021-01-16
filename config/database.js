97c7ff251583b8be437b57fcb8f079f77f132469
capn12 <capn12@gmail.com>
2021-01-16 20:53:51 +0200

    atlas last try

--- config/database.js
+++ config/database.js
@@ -4,17 +4,31 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "localhost"),
-        srv: env.bool("DATABASE_SRV", false),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "next-blog"),
-        username: env("DATABASE_USERNAME", null),
-        password: env("DATABASE_PASSWORD", null),
+        uri: env("DATABASE_URI"),
       },
       options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        ssl: env.bool("DATABASE_SSL", false),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
 });
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         host: env("DATABASE_HOST", "localhost"),
+//         srv: env.bool("DATABASE_SRV", false),
+//         port: env.int("DATABASE_PORT", 27017),
+//         database: env("DATABASE_NAME", "next-blog"),
+//         username: env("DATABASE_USERNAME", null),
+//         password: env("DATABASE_PASSWORD", null),
+//       },
+//       options: {
+//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+//         ssl: env.bool("DATABASE_SSL", false),
+//       },
+//     },
+//   },
+// });
